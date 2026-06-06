d265fea2288dbb732084ea752f2f7a5110c57c53
geethika thogati <252539095+24h71a12c6-ops@users.noreply.github.com>
2026-06-06 12:41:15 +0530

    Updated project files

--- includes/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({ 
-  cloud_name: 'dtorpeosf'.trim(), 
-  api_key: '548469229338858'.trim(), 
-  api_secret: 'Z8OszBAc0pE3g5hQ2nCZ9vLqdvk'.trim() 
-});
-
-module.exports = cloudinary;
